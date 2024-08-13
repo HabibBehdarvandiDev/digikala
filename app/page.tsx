a7833c344a6b2fbe0c2093949a5458ccb001caab
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-08-14 01:02:02 +0330

    setup prisma ORM

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main>تست اولیه</main>
+    <main>تست اولیه
+
+      <Button color="primary" variant="flat">سلام دوستان عزیز</Button>
+    </main>
   );
 }
