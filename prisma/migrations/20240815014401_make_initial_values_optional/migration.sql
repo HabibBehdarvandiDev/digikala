70014284c8faf0437846e9d2004053a5d3647551
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-08-16 23:39:56 +0330

    delete everything

---
+++ prisma/migrations/20240815014401_make_initial_values_optional/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `users` MODIFY `first_name` VARCHAR(100) NULL,
+    MODIFY `last_name` VARCHAR(100) NULL,
+    MODIFY `username` VARCHAR(255) NULL,
+    MODIFY `password` VARCHAR(255) NULL;
